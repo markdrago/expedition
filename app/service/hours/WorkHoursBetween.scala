c336d6199440ceb93eb9e694a1a28a7c6267245c
Mark Drago <markdrago@gmail.com>
2013-07-08 18:29:59 -0400

    add support for holidays

--- app/service/hours/WorkHoursBetween.scala
+++ app/service/hours/WorkHoursBetween.scala
@@ -1,28 +1,38 @@
 package service.hours
 
+import java.util
 import org.joda.time._
+import play.api.Play
+import play.api.Play.current
+import scala.collection.JavaConversions._
 
 //TODO: add support for holidays
 object WorkHoursBetween {
-  //TODO move these constants in to the configuration rather than hard code them here
-  val weekendDays = Set(DateTimeConstants.SATURDAY, DateTimeConstants.SUNDAY)
-  val startTime = new LocalTime(10, 0)
-  val endTime = new LocalTime(18, 0)
-  val millisPerHour = 1000d * 60d * 60d
+  val weekendDays = {
+    val option = Play.configuration.getIntList("application.weekend_days")
+    option.getOrElse(new util.LinkedList()).toSet
+  }
+  val holidays = {
+    val option = Play.configuration.getStringList("application.holidays")
+    option.getOrElse(new util.LinkedList()).toSet.map(LocalDate.parse)
+  }
+  val startTime = LocalTime.parse(Play.configuration.getString("application.start_time").get)
+  val endTime = LocalTime.parse(Play.configuration.getString("application.end_time").get)
 
   def apply(start: String, end: String): Double = apply(Instant.parse(start), Instant.parse(end))
 
   def apply(start: Instant, end: Instant): Double = {
     def days: Stream[LocalDate] = dayStream(start, end)
     def isWeekday(d: LocalDate) = ! weekendDays.contains(d.dayOfWeek().get())
-    def weekdays: Stream[LocalDate] = days.filter(isWeekday)
+    def isNotHoliday(d: LocalDate) = ! holidays.contains(d)
+    def workdays: Stream[LocalDate] = days.filter(isWeekday).filter(isNotHoliday)
     def dayToWorkHourInterval(d: LocalDate) = new Interval(d.toDateTime(startTime), d.toDateTime(endTime))
-    def workHourIntervals: Stream[Interval] = weekdays.map(dayToWorkHourInterval)
+    def workHourIntervals: Stream[Interval] = workdays.map(dayToWorkHourInterval)
 
     val entire_interval = new Interval(start, end)
     val overlapping_intervals = workHourIntervals.map(entire_interval.overlap(_))
     val total_millis = overlapping_intervals.foldLeft[Long](0)((acc, inter) => { acc + inter.toDuration.getMillis })
-    total_millis / millisPerHour
+    total_millis / DateTimeConstants.MILLIS_PER_HOUR.toDouble
   }
 
   def dayStream(start: Instant, end: Instant) = {
